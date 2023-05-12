6ac7cd74c7ba417ffde976eb1564fbc8ff0f31dc
harshitha <sanjivaniharshitha3@gmail.com>
2023-05-12 12:56:07 -0400

    12th may bug fixed

--- src/test/java/TestCases/TC_001_AccountRegistrationTest.java
+++ src/test/java/TestCases/TC_001_AccountRegistrationTest.java
@@ -1,5 +1,36 @@
 package TestCases;
 
-public class TC_001_AccountRegistrationTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+import PageObjects.Registerpage;
+import testBase.BaseClass;
+
+public class TC_001_AccountRegistrationTest extends BaseClass{
+  @Test
+  public void Registration() {
+	  try
+	  {
+	  Registerpage rp= new Registerpage(driver);
+	  rp.lnk();
+	  rp.setfirstname("sai");
+	  rp.setlasttname("s");
+	  rp.setaddress("123  street");
+	  rp.setcity("richmond");
+	  rp.setstate("ontario");
+	  rp.setzipcode("ad456");
+	  rp.setphoneum("1237895679");
+	  rp.setssnumber("34589675");
+	  rp.setusername("sais@abc.com");
+	  rp.setpassword("password");
+	  rp.setrepeatpassword("password");
+	  rp.btton();
+	  
+		  
+	  }
+	  catch (Exception e)
+	  {
+		  Assert.fail();
+	  }
+  }
 }
--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -6,37 +6,31 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Parameters;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
 
-	
-	
-	
-	
-	public  static WebDriver driver;
+public  static WebDriver driver;
 	
 	
 	@BeforeClass
-	@Parameters("browser")
-	public void setup(String br) {
+
+	public void setup() {
 		
 		
 		ChromeOptions ops=new ChromeOptions();
 		ops.addArguments("--remote-allow-origins=*");
-		if(br.equals("chrome"))
+		/*if(br.equals("chrome"))
 		{
 			driver=new ChromeDriver();
-		}
+		}*/
 	
 	WebDriverManager.chromedriver().setup();
 	driver=new ChromeDriver(ops);
 	driver.manage().deleteAllCookies();
 	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	driver.manage().window().maximize();
-	driver.get("appURL");
+	driver.get("https://parabank.parasoft.com/parabank/index.htm");
 }
 	
 	@AfterClass
