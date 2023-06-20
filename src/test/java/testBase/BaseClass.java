113860df49afd4f241e5cb12dfb3b67570993d91
harshitha <sanjivaniharshitha3@gmail.com>
2023-06-20 11:44:36 -0400

    config files added

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -1,6 +1,7 @@
 package testBase;
 
 import java.time.Duration;
+import java.util.ResourceBundle;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
@@ -10,32 +11,41 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
 
 public  static WebDriver driver;
-public Logger logger; //for logging
+public Logger logger;//for logging
+public ResourceBundle rb;
 	
 	
 	@BeforeClass
-
-	public void setup() {
+    @Parameters("browser")
+	public void setup(String br) {
 		
 		logger=LogManager.getLogger(this.getClass());
+		rb=ResourceBundle.getBundle("config");
 		ChromeOptions ops=new ChromeOptions();
 		ops.addArguments("--remote-allow-origins=*");
-		/*if(br.equals("chrome"))
+		if(br.equals("chrome"))
 		{
 			driver=new ChromeDriver();
-		}*/
-	
+		}
+		/*else
+			if(br.equals("edge"))
+			{
+				driver=new EdgeDriver();
+			}
+	*/
 	WebDriverManager.chromedriver().setup();
 	driver=new ChromeDriver(ops);
 	driver.manage().deleteAllCookies();
 	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	driver.manage().window().maximize();
-	driver.get("https://parabank.parasoft.com/parabank/index.htm");
+	driver.get(rb.getString("appURL"));
 }
 	
 	@AfterClass
